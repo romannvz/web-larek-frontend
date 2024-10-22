3849fdf8ceb405a321e44bc3873b0377bf981504
R V Laptop <romannvz@ya.ru>
2024-10-22 13:48:07 +0800

    before 2nd review (8)

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,59 +3,20 @@ interface IItem {
     category: string,
     title: string,
     description: string,
-    price: number,
+    price: number | null,
     image: string
+    displayItems(): void    
 }
 
 interface IAPI {
     baseURL: string,
-    getItemsFromAPI: void,
-    displayItems: void
+    config: []
+    getItemsFromAPI(): Promise<[]>,
 }
 
 interface Modal {
-    body: Array<string>,
-    submitAction: void,
-    closeModal: void
-}
-
-class API implements IAPI {
-    //здесь указываются параметры подключения, like :
-    // const config = {
-    //     baseUrl: "https://larek-api.nomoreparties.co",
-    //     headers: {
-    //       "Content-Type": "application/json",
-    //     },
-    // };
-    //происходит подключение к апи, парсится информация о всех карточках и возвращается массивом объектов карточек, которые впоследствии передаются в класс Item
-}
-
-class Item implements IItem {
-    itemInfo: Array<string>;
-
-    constructor() {
-
-    }
-    //класс получает на входе массив объектов карточек из апи, далее путём array.foreach() передаёт готовую под вывод карточку
-
-}
-
-class createCard {
-    //использует подготовленные данные в Item и выводит на страницу конкретную карточку
-}
-
-class EventEmitter {
-    //устанавливает слушатели на кнопки сабмита, а также следит за открытием нужных модалок в нужный момент. аналогично следит за закрытием всего перечисленного
-}
-
-class openModal implements Modal{
-    //класс, принимающий конкретную модалку и открывающий её. также здесь создаётся коллбек на закрытие модалки
-}
-
-class closeModal {
-    //закрывает конкретную модалку
-}
-
-class validation {
-    //логика валидации всех форм и textInput'ов
+    modal: HTMLElement,
+    body: Array<HTMLElement>,
+    submitAction(): void,
+    closeModal(): void
 }
