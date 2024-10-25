9f5885661c1577b27cbdc12acd3451f4dddd6d5b
R V Laptop <romannvz@ya.ru>
2024-10-25 13:34:27 +0800

    before 3nd review (8)

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,22 +1,42 @@
-interface IItem {
-    id: string,
-    category: string,
-    title: string,
-    description: string,
-    price: number | null,
-    image: string
-    displayItems(): void    
+export interface IProduct {
+    id: string;
+    description: string;
+    image: string;
+    title: string;
+    category: string;
+    price: number | null;
 }
 
-interface IAPI {
-    baseURL: string,
-    config: []
-    getItemsFromAPI(): Promise<[]>,
+export interface IProductsData {
+    products: IProduct[];
+    currentId: Pick<IProduct, 'id'> | null;
+    addProduct(product: IProduct): void;
+    deleteProduct(product: IProduct, action: Function | null): void;
+    getProduct(productId: Pick<IProduct, 'id'>): IProduct;
 }
 
-interface Modal {
-    modal: HTMLElement,
-    body: Array<HTMLElement>,
-    submitAction(): void,
-    closeModal(): void
+export type TProductsMainPage = Pick<IProduct, 'category' | 'title' | 'image' | 'price'>
+
+export type TProductShowMore = Pick<IProduct, 'category' | 'title' | 'description' | 'image' | 'price'>
+
+export type TProductInBasket = Pick<IProduct, 'title' | 'price'>
+
+export interface IClientInfo {
+    paymentMethod: string;
+    address: string;
+    email: string;
+    phone: string;
+    getInfo(): IClientInfo;
+    setInfo(clientData: IClientInfo): void;
+    checkValidation(data: Record<keyof IClientInfo, string>): boolean;
+}
+
+export interface IOrder {
+    payment: Pick<IClientInfo, 'paymentMethod'>;
+    email: Pick<IClientInfo, 'email'>;
+    phone: Pick<IClientInfo, 'phone'>;
+    address: Pick<IClientInfo, 'address'>;
+    total: Pick<IProduct, 'price'>;
+    items: Pick<IProduct, 'id'>[];
+    sendOrder(): void;
 }
